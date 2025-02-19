d9bdbe9d41f05e876ba02ac3537dd8abe5f72564
Dastageer <dastageerh@Dastageers-MacBook-Air-2.local>
2025-02-20 03:25:06 +0530

    test

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = "https://dastageerhc-gh-demo-pro-61.deno.dev:3200"; // Change this if backend is deployed
+const SOCKET_URL = ""; // Change this if backend is deployed
 
 const socket = io(SOCKET_URL, {
   transports: ["websocket"],
